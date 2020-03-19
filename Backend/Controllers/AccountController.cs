cea8fcf80bc4b98fbd34e2cb95162a96a24dc8d6
Musk <chuang.master@gmail.com>
2020-03-20 07:51:59 +0800

    add account controller

---
+++ Backend/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Backend.Controllers
+{
+    public class AccountController : BaseController
+    {
+        [HttpGet]
+        [Route("~/Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("~/Login")]
+        public ActionResult DoLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("~/Sign")]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
